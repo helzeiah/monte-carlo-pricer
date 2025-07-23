b17cf6708a53fb67831feebca053eeace2a83aa0
helzeiah <hdoftw@gmail.com>
2025-07-22 21:04:29 -0400

    Added << overloader.

--- src/Pricer.cpp
+++ src/Pricer.cpp
@@ -1,5 +1,20 @@
-//
-// Created by hdopo on 7/22/2025.
-//
-
 #include "Pricer.h"
+
+#include <iomanip>
+
+std::ostream& operator<<(std::ostream& os, const Pricer& pricer) {
+  os << pricer.getPricingMethod() << ":\n";
+  os << pricer.getOption().toString();
+
+  if (pricer.isPriceCalculated()) {
+    os << "Results:\n"
+       << "Price: $" << std::fixed << std::setprecision(2) << pricer.getPrice()
+       << "\n"
+       << "Calculation time: " << std::fixed << std::setprecision(4)
+       << pricer.getLastCalculationTime() << "s\n";
+  } else {
+    os << "Status:\n"
+       << "Price: Not calculated (call calculatePrice())\n";
+  }
+  return os;
+}
