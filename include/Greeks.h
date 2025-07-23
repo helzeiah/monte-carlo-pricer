1d9eb2955101e6ce4a536fe0701dba49d61b49b8
helzeiah <hdoftw@gmail.com>
2025-07-22 21:01:29 -0400

    Added << overloader to Greeks.

--- include/Greeks.h
+++ include/Greeks.h
@@ -1,8 +1,39 @@
-//
-// Created by hdopo on 7/22/2025.
-//
-
 #ifndef GREEKS_H
 #define GREEKS_H
 
-#endif //GREEKS_H
+/**
+ * Represents the Greeks parameters often used in financial derivatives pricing
+ * models. These parameters provide sensitivity measures of the derivative.
+ */
+struct Greeks {
+  double delta{0.0};  // ∂V/∂S
+  double gamma{0.0};  // ∂²V/∂S²
+  double theta{0.0};  // ∂V/∂t
+  double vega{0.0};   // ∂V/∂σ
+  double rho{0.0};    // ∂V/∂r
+
+  Greeks() = default;
+  Greeks(double d, double g, double t, double v, double r)
+      : delta(d), gamma(g), theta(t), vega(v), rho(r) {}
+
+  /**
+   * Overloads the stream insertion operator for the Greeks struct.
+   * Outputs the values of the Greek parameters in a formatted string.
+   *
+   * @param os The output stream where the information is to be written.
+   * @param greeks The Greeks object containing the delta, gamma, theta, vega,
+   * and rho values to be displayed.
+   * @return A reference to the modified output stream with the formatted Greeks
+   * information inserted.
+   */
+  friend std::ostream& operator<<(std::ostream& os, const Greeks& greeks) {
+    os << "Greeks: [δ=" << std::fixed << std::setprecision(4) << greeks.delta
+       << " γ=" << greeks.gamma
+       << " θ=" << greeks.theta
+       << " ν=" << greeks.vega
+       << " ρ=" << greeks.rho << "]";
+    return os;
+  }
+};
+
+#endif  // GREEKS_H
